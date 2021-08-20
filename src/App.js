701e1626a7542abd2d8a1e5b6c301e8df967ee98
HimanshuUdhwani6 <udhwani.himanshu6@gmail.com>
2021-08-21 00:19:57 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+import {Component, useEffect, useState,} from "react";
+import Login from "./components/Login/Login";
+import {BrowserRouter, Route, Switch, useHistory} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Hostel from "./components/Hostel/Hostel";
+import ChooseHostel from "./components/chooseHostel/chooseHostel";
+import ChooseRoom from "./components/chooseRoom/chooseRoom";
+import Booked from "./components/booked/booked";
+
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+});
+
+
+const App = () => {
+
+    return (
+
+        <BrowserRouter>
+            <Switch>
+                <main className="d-flex justify-content-center align-items-center main-container">
+                    <Route exact path={"/"} component={Hostel}/>
+                    <Route path={"/login"} component={Login}/>
+                    <Route path={"/choose-hostel"} component={ChooseHostel}/>
+                    <Route path={"/choose-room"} component={ChooseRoom}/>
+                    <Route path={"/booked"} component={Booked}/>
+                </main>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from "firebase";
+import {useHistory} from "react-router-dom";
+
+
+const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: () => false,
+    },
+};
+
+const Login = ()=>{
+    const [isSigned, setIsSigned] = useState(false);
+    let history = useHistory();
+
+    useEffect(async () => {
+        await firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                setIsSigned(true);
+                if (window.location.pathname === "/login") {
+                    history.push("/");
+                }
+            }
+        })
+    }, [])
+
+    return (
+        <div className="">
+            <div className="text-center">
+                <h4>Hostel Allocation</h4>
+                <p>Please sign-in:</p>
+            </div>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+    );
+}
+
+export default Login;
